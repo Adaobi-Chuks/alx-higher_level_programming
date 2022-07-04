bd4289048153385c5c6918895927b4675c683276
Adaobi Chuks-Aginam <chuksaginamada@gmail.com>
2022-07-04 23:57:01 +0100

    completed task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 def uppercase(str):
+    new_string = ''
+    i = 0
     for char in str:
-        if (str >=90):
-            print('{:s}'.format(chr((ord(char))-32)), end='')
-    print()
+        if (ord(char) >= 97):
+            num = (ord(char))-32
+            char1 = chr(num)
+            new_string += char1
+            i += 1
+        else:
+            new_string += char
+    print(new_string)
