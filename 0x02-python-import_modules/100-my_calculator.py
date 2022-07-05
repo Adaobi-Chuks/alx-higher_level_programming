979c4d5b1702b2e64e6715ddd87ee73b946ca6fd
Adaobi Chuks-Aginam <chuksaginamada@gmail.com>
2022-07-05 19:42:09 +0100

    task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
     import sys
     
     if (len(sys.argv) - 1) != 3:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         sys.exit(1)
         
-    if ((sys.argv[2] != '+') and (sys.argv[2] != '-') and (sys.argv[2] != '*') and (sys.argv[2] != '/')):
+    operator = sys.argv[2]    
+    if (operator != '+') and (operator != '-') and (operator != '*') and (operator != '/'):
         print('Unknown operator. Available operators: +, -, * and /')
         sys.exit(1)
         
+    from calculator_1 import add, sub, mul, div
     a = sys.argv[1]
     b = sys.argv[3]
-    operator = sys.argv[2]
     
     if operator == '+':
         print(f'{a} {operator} {b} = {add(a, b)}')
