f2f2d36a294f45f0a78ffddeb338a2ac1b86b9ad
Adaobi Chuks-Aginam <chuksaginamada@gmail.com>
2022-07-05 08:46:08 +0100

    task 15

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def remove_char_at(str, n):
+    new_string = ''
+    position = -1
+    for char in str:
+        position += 1
+        if position == n:
+            continue
+        new_string += char
+    return new_string
