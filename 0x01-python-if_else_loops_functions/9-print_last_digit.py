9c688feafda09df198f8d13f068bfca975a2b7b5
Adaobi Chuks-Aginam <chuksaginamada@gmail.com>
2022-07-05 08:05:57 +0100

    correction for task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    print('{}'.format(number % 10), end='')
+    if number >= 0:
+        l_digit = number % 10
+    else:
+        l_digit = number % -10
+    print("{:d}".format(l_digit), end='')
+    return (l_digit)
