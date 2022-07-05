61174e99bdcfbdd9451c54f2555360f2f65508e4
Adaobi Chuks-Aginam <chuksaginamada@gmail.com>
2022-07-05 08:35:58 +0100

    task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+count = 2
+lower = 122
+upper = 90
+for i in range(26):
+    if (count % 2) == 0:
+        char = chr(lower)
+    else:
+        char = chr(upper)
+    count += 1
+    lower -= 1
+    upper -= 1
+    print('{}'.format(char), end='')
