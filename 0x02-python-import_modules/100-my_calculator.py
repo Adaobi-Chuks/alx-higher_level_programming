0e995a5e0c9e88237d17a455f59cf72c607c6d30
Adaobi Chuks-Aginam <chuksaginamada@gmail.com>
2022-07-05 19:35:27 +0100

    corrections for task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import calculator_1,sys
+    import calculator_1, sys
     
     if (len(sys.argv) - 1) != 3:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
@@ -15,10 +15,10 @@ if __name__ == "__main__":
     operator = sys.argv[2]
     
     if operator == '+':
-        print(f'{a} {operator} {b} = {a + b}')
+        print(f'{a} {operator} {b} = {calculator_1.add(a, b)}')
     elif operator == '-':
-        print(f'{a} {operator} {b} = {a - b}')
+        print(f'{a} {operator} {b} = {calculator_1.sub(a, b)}')
     elif operator == '*':
-        print(f'{a} {operator} {b} = {a * b}')
+        print(f'{a} {operator} {b} = {calculator_1.mul(a, b)}')
     elif operator == '/':
-        print(f'{a} {operator} {b} = {a / b}')    
+        print(f'{a} {operator} {b} = {calculator_1.div(a, b)}')    
