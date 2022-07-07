1599e8ddc37d394f6a4e7be32ef162dbeb485dcc
Adaobi Chuks-Aginam <chuksaginamada@gmail.com>
2022-07-07 14:22:33 +0100

    correction

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if type(a_dictionary) == dict and type(key) == str:
-        a_dictionary[key] = value
-        return a_dictionary
+    a_dictionary[key] = value
+    return a_dictionary
