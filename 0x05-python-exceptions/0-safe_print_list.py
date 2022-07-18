e080e9f0cee7beafdcbf7be8b447224dc12ab5be
Adaobi Chuks-Aginam <chuksaginamada@gmail.com>
2022-07-18 18:20:54 +0100

    corrections for task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     temp = 0
-    for i in my_list:
-        temp += 1
-    print(temp)
-    if x <= temp:
+    try:
         for i in range(x):
-            print(my_list[i], end="")
+            print(my_list[i], end='')
+            temp += 1
+        print()
+        return temp
+    except Exception:
+        print()
+        return temp
